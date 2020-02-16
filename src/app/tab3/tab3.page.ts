95ede002729f79ba42a26bb83a1568ada0780532
Niklas Merz <niklasmerz@linux.com>
2020-02-16 11:27:08 +0100

    Port pecuniator script to app

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,27 +1,92 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FileCacheService, CachedFile } from '../services/file-cache.service';
+import { PecuniAPI, PEntry } from '@merzlabs/pecuniator-api';
+
+class CheckEntry extends PEntry {
+    found: Array<string>;
+}
+
 @Component({
     selector: 'app-tab3',
     templateUrl: 'tab3.page.html',
     styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page implements OnInit {
-    readonly stringToParse: string;
-    text: string;
-    error: string;
-    editorOptions = { theme: 'vs-dark', language: 'javascript' };
-    code = '';
-    worker: Worker;
-    runBinding: any;
-    files: CachedFile[];
+export class Tab3Page {
+    private files: CachedFile[];
+    api: PecuniAPI;
+    message = '';
 
-    constructor(private filecache: FileCacheService, private cd: ChangeDetectorRef) {}
+    constructor(private filecache: FileCacheService) {
+        this.api = new PecuniAPI();
+    }
 
-    ionViewDidEnter() {
+    ionViewWillEnter() {
+        // Reset api instance of this page from files in cache every time
+        this.api.clear();
         this.files = this.filecache.getAll();
+        if (typeof this.files !== 'undefined' && this.files.length > 0) {
+            for (const file of this.files) {
+                this.api.load(file.content);
+            }
+        }
+
+        const categories = {
+            saving: {
+                remittanceInformation: ['DEPOT'],
+                amount: 0,
+                title: 'Savings'
+            },
+            fuel: {
+                creditorName: ['Tank'],
+                amount: 0,
+                title: 'Tank'
+            },
+            insu: {
+                remittanceInformation: ['Versicherung'],
+                amount: 0,
+                title: 'Insurance'
+            },
+            online: {
+                creditorName: ['Paypal'],
+                amount: 0,
+                title: 'Online'
+            },
+            cell: {
+                creditorName: ['Drillisch', 'Telekom'],
+                amount: 0,
+                title: 'Cellphone'
+            },
+        };
+        for (const entry of this.api.entries) {
+            const checkEntry = entry as CheckEntry;
+            checkEntry.found = [];
+            this.checkCategories(checkEntry, categories);
+        }
+
+        console.debug(categories);
+        for (const name in categories) {
+            if (categories.hasOwnProperty(name)) {
+                const cat = categories[name];
+                this.message += `|| ${cat.title} - ${cat.amount}€ ||`;
+            }
+        }
     }
 
-    ngOnInit() {
+    checkCategories(entry: CheckEntry, categories) {
+        for (const catname in categories) {
+            if (categories.hasOwnProperty(catname)) {
+                const cat = categories[catname];
+                for (const check in cat) {
+                    if (check !== 'amount' && check !== 'title' && typeof entry[check] !== 'undefined') {
+                        for (const test of cat[check]) {
+                            if (!entry.found.includes(catname) && entry[check].toString().toLowerCase().includes(test.toLowerCase())) {
+                                cat.amount += entry.amount;
+                                entry.found.push(catname);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-
 }
