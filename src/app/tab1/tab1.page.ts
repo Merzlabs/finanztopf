6cdd13d9cf9e9c882a345e204ee247d5fa1a1d19
Niklas Merz <niklasmerz@linux.com>
2020-02-23 10:56:13 +0100

    Implement XS2A local testing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FileCacheService, CachedFile } from '../services/file-cache.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
   fileNames: string;
 
-  constructor(private filecache: FileCacheService) {
+  constructor(private filecache: FileCacheService, private route: ActivatedRoute) {
     this.fileNames = '';
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((res) => {
+      if (res.state) {
+        this.loadxs2aTransactions(res.state);
+      }
+    });
   }
 
 
@@ -44,5 +53,25 @@ export class Tab1Page {
       this.filecache.add(new CachedFile(file.name, xmlContent));
     };
     xmlReader.readAsText(file);
+  }
+
+  async connectToBank() {
+    const response = await fetch('http://localhost:8080/oauth/start');
+    const body = await response.json();
+    location.href = body.authUrl;
+  }
+
+  async loadxs2aTransactions(state: string) {
+    let response = await fetch('http://localhost:8080/accounts?state=' + state);
+    let body = await response.json();
+    const accountList = body.accounts;
+
+    // Load all accounts
+    for (const account of accountList) {
+      response = await fetch(`http://localhost:8080/accounts/transactions?state=${state}&resourceId=${account.resourceId}`);
+      body = await response.text();
+      console.log(body);
+      this.filecache.add(new CachedFile(account.resourceId + '.xml', body));
+    }
   }
 }
