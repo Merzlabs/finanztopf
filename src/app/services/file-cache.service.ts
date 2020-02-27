541ee0957117ae6716aea109af6dfcb92ad2110d
DavidM42 <davidmerz@gmx.de>
2020-02-27 21:33:08 +0100

    init of working zip load in for xml files * also exported more to service not controllers

--- src/app/services/file-cache.service.ts
+++ src/app/services/file-cache.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import * as JSZip from 'jszip';
+
 export class CachedFile {
   constructor(public name: string, public content: string) {}
 }
@@ -14,16 +16,61 @@ export class FileCacheService {
     this.files = [];
   }
 
+  get fileNames() {
+    return this.files.map((elem) => elem.name);
+  }
+
+  get fileNameStringConcat() {
+    if (this.fileNames.length === 0) {
+      return '';
+    }
+    return this.fileNames.join(',');
+  }
+
+  async loadFiles(files: FileList) {
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type === 'text/xml') {
+        this.processXMLFile(files[i]);
+      } else if (files[i].type === 'application/zip') {
+        const newFileZipObject = await JSZip.loadAsync(files[i]);
+
+        newFileZipObject.forEach(async (path, file) => {
+          // filename check not ideal only temp solution till find out type other way
+          if (path.endsWith('.xml')) {
+            const xmlContent: string = await file.async('string');
+            let name = path;
+
+            const splitPath = path.split('/');
+            if (splitPath.length > 0) {
+              name = splitPath[splitPath.length - 1];
+            }
+            this.add(new CachedFile(name, xmlContent));
+          } else {
+            console.warn('Unzipped File not supported');
+          }
+        });
+      } else {
+        console.warn('File not supported');
+      }
+    }
+  }
+
+  private processXMLFile(file: File) {
+    const xmlReader = new FileReader();
+    xmlReader.onload = (e: any) => {
+      const xmlContent = e.target.result;
+      this.add(new CachedFile(file.name, xmlContent));
+    };
+    xmlReader.readAsText(file);
+  }
+
   add(file: CachedFile) {
     this.files.push(file);
   }
 
   getAll() {
     return this.files;
-  }
-
-  getFileNames() {
-    return this.files.map((elem) => elem.name);
   }
 
   deleteAll() {
