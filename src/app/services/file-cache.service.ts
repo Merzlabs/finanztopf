c21b36bd700b2b1757d18428aa62a045d4929bae
Niklas Merz <niklasmerz@linux.com>
2020-02-29 10:09:00 +0100

    Improve console output

--- src/app/services/file-cache.service.ts
+++ src/app/services/file-cache.service.ts
@@ -47,11 +47,11 @@ export class FileCacheService {
             }
             this.add(new CachedFile(name, xmlContent));
           } else {
-            console.warn('Unzipped File not supported');
+            console.warn('Unzipped File not supported', path);
           }
         });
       } else {
-        console.warn('File not supported');
+        console.warn('File not supported:', files[i].name);
       }
     }
   }
