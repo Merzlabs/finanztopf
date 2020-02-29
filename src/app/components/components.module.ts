d12b6a896e2327cf86d5823904f814c3c40ceb42
Niklas Merz <niklasmerz@linux.com>
2020-02-29 13:47:58 +0100

    Rename entry list component folder

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -8,7 +8,7 @@ import { OpensourceComponent } from './opensource/opensource.component';
 import { SumOverviewComponent } from './sum-overview/sum-overview.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { EntrySearchComponent } from './entry-search/entry-search.component';
-import { EntryListComponent } from './entry-list-item/entry-list.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
   imports: [
